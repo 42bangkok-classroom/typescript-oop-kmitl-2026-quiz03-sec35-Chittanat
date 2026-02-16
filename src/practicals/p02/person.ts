bc89ec58c174f0d9950806e4cf53ab4bba2f2110
jayler <jittanadunited@gmail.com>
2026-02-16 10:13:41 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,26 @@
-export class Person {}
+export class Person {
+   firstname?:string
+   lastname?: string
+   private age : number = 0; 
+   static COUNTRY :string = "Thailand"
+
+    constructor(firstname ?: string , lastname ?: string){
+        if (firstname)
+            this.firstname = firstname
+        if (lastname)
+            this.lastname = lastname
+    }
+
+   setAge(age:number){
+    if (age > 0 && age <= 100) this.age = age 
+    return this.age = age
+   }
+
+   getAge(age:number){
+    return this.age
+   }
+  getFullName(firstname:string, lastname:string){
+    return `${this.firstname}  ${this.lastname}`
+  }
+
+}
