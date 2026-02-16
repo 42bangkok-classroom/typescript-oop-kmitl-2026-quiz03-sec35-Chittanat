3817dc844c7e19fa6eb379b0df2b8424ca30cba5
jayler <jittanadunited@gmail.com>
2026-02-16 09:39:41 +0700

    p01

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,8 +1,8 @@
 export class Student {
-    firstname:string;
-    lastname:string;
+    firstname?:string;
+    lastname?:string;
 
-    constructor(firstname:string, lastname:string){
+    constructor(firstname?:string, lastname?:string){
         this.firstname = firstname
         this.lastname = lastname
     }
